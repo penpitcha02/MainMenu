9105b2890207b26746194f6ea26166bed1904056
PENPITCHA SAPPAKUN <63010709@KMITL.AC.TH>
2020-10-31 16:48:36 +0700

    1

--- MainMenu/Source.cpp
+++ MainMenu/Source.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(1080, 720), "Main Menu");
 
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.display();
+	}
+	return 0;
 }
